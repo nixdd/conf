babec439bcecc7a28467169bac433222d7434764
nixdd <41232543+nixdd@users.noreply.github.com>
2018-09-03 14:13:29 +0200

    Create general.scm

---
+++ guixsd/general.scm
@@ -0,0 +1,27 @@
+(specifications->manifest
+  '(
+    "pcmanfm"
+    "rofi"
+
+    "git"
+
+    "icecat"
+
+    "vim-full"
+    "libreoffice"
+    "texlive"
+    "texmaker"
+    "dia"
+    "aspell"
+
+    "gimp"
+    "inkscape"
+    "blender"
+
+    "audacity"
+
+    "r"
+    "units"
+    "octave"
+    "gnuplot"
+  ))
